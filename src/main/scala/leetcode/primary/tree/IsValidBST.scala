34c3d476972e44b225fe5eb84c3c6ffdacdd1492
Leibniz.Hu <leibnizhu@gmail.com>
2018-06-07 09:09:26 +0800

    Judge if a tree is binary search tree

---
+++ src/main/scala/leetcode/primary/tree/IsValidBST.scala
@@ -0,0 +1,27 @@
+package leetcode.primary.tree
+
+/**
+  * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/48/
+  *
+  * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+  * 一个二叉搜索树具有如下特征：
+  * 节点的左子树只包含小于当前节点的数。
+  * 节点的右子树只包含大于当前节点的数。
+  * 所有左子树和右子树自身必须也是二叉搜索树。
+  */
+object IsValidBST {
+  def isValidBST(root: TreeNode): Boolean = {
+    /**
+      * 递归,满足BST的条件:左树是bst,右树是bst,当前节点在min max 范围内
+      *
+      * @param cur 当前节点
+      * @param min 当前最小值(不包含)
+      * @param max 当前最大值(不包含)
+      * @return 当前节点下面的树是否二叉搜索树
+      */
+    def isBinarySearchTree(cur: TreeNode, min: Long, max: Long): Boolean =
+      if (cur == null) true else
+        cur.value > min && cur.value < max && isBinarySearchTree(cur.left, min, cur.value) && isBinarySearchTree(cur.right, cur.value, max)
+    isBinarySearchTree(root, Int.MinValue - 1L , Int.MaxValue + 1L)
+  }
+}
---
+++ src/test/scala/leetcode/primary/tree/IsValidBSTTEst.scala
@@ -0,0 +1,52 @@
+package leetcode.primary.tree
+
+import org.scalatest.FunSuite
+
+
+class IsValidBSTTEst extends FunSuite{
+
+  /**
+    *   2
+    *  / \
+    * 1   3
+    */
+  test("2,1,3"){
+    check(Array[Integer](2,1,3), shouldBe = true)
+  }
+
+  /**
+    *   5
+    *  / \
+    * 1   4
+    *    / \
+    *   3   6
+    * 解释: 输入为: [5,1,4,null,null,3,6]。
+    * 根节点的值为 5 ，但是其右子节点值为 4 。
+    */
+  test("5,1,4,null,null,3,6"){
+    check(Array[Integer](5,1,4,null,null,3,6), shouldBe = false)
+  }
+
+  test("2"){
+    check(Array[Integer](2), shouldBe = true)
+  }
+
+  test("2,2,2"){
+    check(Array[Integer](2,2,2), shouldBe = false)
+  }
+
+  test("2147483647"){
+    check(Array[Integer](2147483647), shouldBe = true)
+  }
+
+  test("-2147483648"){
+    check(Array[Integer](-2147483648), shouldBe = true)
+  }
+
+  def check(arr: Array[Integer], shouldBe: Boolean): Unit = {
+    val tree = TreeNode.buildTree(arr)
+    val result = IsValidBST.isValidBST(tree)
+    assert(result == shouldBe)
+  }
+
+}
