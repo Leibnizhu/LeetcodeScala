010148dd3cb690f179b5989d750c96ab31d7d501
Leibniz.Hu <leibnizhu@gmail.com>
2018-06-07 12:50:14 +0800

    Level-ordered traverse a tree

---
+++ src/main/scala/leetcode/primary/tree/LevelOrderTraverse.scala
@@ -0,0 +1,34 @@
+package leetcode.primary.tree
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/50/
+  *
+  * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+  */
+object LevelOrderTraverse {
+  /**
+    * 从树根开始递归,传入存储结果的二维List和深度
+    * 深度-1对应二维List的第一层下标
+    * 递归的时候先把当前节点的值放入对应深度的一维List
+    * 然后递归调用左右子树的,深度+1
+    *
+    * @param root
+    * @return
+    */
+  def levelOrder(root: TreeNode): List[List[Int]] = {
+    def traverse(cur: TreeNode, depth: Int, res: ListBuffer[ListBuffer[Int]]): Unit = {
+      if (cur != null) { // null的情况即叶子下一级,不考虑
+        if (depth > res.size) res += ListBuffer[Int]() //增加新List
+        res(depth - 1) += cur.value //当前节点的值放入对应深度的一维List
+        traverse(cur.left, depth + 1, res)
+        traverse(cur.right, depth + 1, res)
+      }
+    }
+
+    var result: ListBuffer[ListBuffer[Int]] = ListBuffer()
+    traverse(root, 1, result)
+    result.map(_.toList).toList
+  }
+}
---
+++ src/test/scala/leetcode/primary/tree/LevelOrderTraverseTest.scala
@@ -0,0 +1,63 @@
+package leetcode.primary.tree
+
+import org.scalatest.FunSuite
+
+class LevelOrderTraverseTest extends FunSuite{
+  /**
+  *     1
+  *    / \
+  *   2   2
+  *  / \ / \
+  * 3  4 4  3
+  */
+  test("1,2,2,3,4,4,3"){
+    check(Array[Integer](1,2,2,3,4,4,3), shouldBe = List(List(1),List(2,2),List(3,4,4,3)))
+  }
+
+  /**
+    *    1
+    *   / \
+    *  2   2
+    *   \   \
+    *   3    3
+    */
+  test("1,2,2,null,3,null,3"){
+    check(Array[Integer](1,2,2,null,3,null,3), shouldBe = List(List(1),List(2,2),List(3,3)))
+  }
+
+  /**
+    *     1
+    *   /  \
+    *  2    2
+    *   \  /
+    *   3 3
+    */
+  test("1,2,2,null,3,3"){
+    check(Array[Integer](1,2,2,null,3,3), shouldBe = List(List(1),List(2,2),List(3,3)))
+  }
+
+  test("2,2,2"){
+    check(Array[Integer](2,2,2), shouldBe = List(List(2),List(2,2)))
+  }
+
+  test("1,2,3"){
+    check(Array[Integer](1,2,3), shouldBe = List(List(1),List(2,3)))
+  }
+
+  test("2147483647"){
+    check(Array[Integer](2147483647), shouldBe = List(List(2147483647)))
+  }
+
+  test("-2147483648"){
+    check(Array[Integer](-2147483648), shouldBe = List(List(-2147483648)))
+  }
+
+  def check(arr: Array[Integer], shouldBe: List[List[Int]]): Unit = {
+    val tree = TreeNode.buildTree(arr)
+    val result = LevelOrderTraverse.levelOrder(tree)
+    assert(result.size == shouldBe.size)
+    for(i <- result.indices){
+      assert(result(i) == shouldBe(i))
+    }
+  }
+}
