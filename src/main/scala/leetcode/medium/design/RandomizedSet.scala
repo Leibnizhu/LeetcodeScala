a06d26d60d0fb1ccdc795ec311bdb9b66a562f50
Leibniz.Hu <leibnizhu@gmail.com>
2018-06-20 11:59:16 +0800

    random set

---
+++ src/main/scala/leetcode/medium/design/RandomizedSet.scala
@@ -0,0 +1,46 @@
+package leetcode.medium.design
+
+import scala.collection.mutable
+import scala.util.Random
+
+/**
+  * https://leetcode-cn.com/explore/interview/card/top-interview-questions-medium/52/design/110/
+  *
+  * 设计一个支持在平均 时间复杂度 O(1) 下，执行以下操作的数据结构。
+  *  - insert(val)：当元素 val 不存在时，向集合中插入该项。
+  *  - remove(val)：元素 val 存在时，从集合中移除该项。
+  *  - getRandom：随机返回现有集合中的一项。每个元素应该有相同的概率被返回。
+  */
+class RandomizedSet {
+  /** Initialize your data structure here. */
+  val set = new mutable.HashSet[Int]()
+  val rand = new Random()
+
+  /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+  def insert(`val`: Int): Boolean = {
+    if(set.contains(`val`))
+      false
+    else{
+      set += `val`
+      true
+    }
+  }
+
+  /** Removes a value from the set. Returns true if the set contained the specified element. */
+  def remove(`val`: Int): Boolean = {
+    if(!set.contains(`val`))
+      false
+    else {
+      set -= `val`
+      true
+    }
+  }
+
+  /** Get a random element from the set. */
+  def getRandom(): Int = {
+    val step = rand.nextInt(set.size)
+    val itr = set.iterator
+    for(i <- 0 until  step) itr.next()
+    itr.next()
+  }
+}
---
+++ src/test/scala/leetcode/medium/design/RandomizedSetTest.scala
@@ -0,0 +1,31 @@
+package leetcode.medium.design
+
+import org.scalatest.FunSuite
+
+class RandomizedSetTest extends FunSuite{
+  test("sample"){
+    // 初始化一个空的集合。
+    val set = new RandomizedSet()
+    // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+    assert(set.insert(1))
+    // 返回 false ，表示集合中不存在 2 。
+    assert(!set.remove(2))
+    // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+    set.insert(2)
+    // getRandom 应随机返回 1 或 2 。
+    var rand = set.getRandom()
+    assert(rand == 1 || rand == 2)
+    rand = set.getRandom()
+    assert(rand == 1 || rand == 2)
+    rand = set.getRandom()
+    assert(rand == 1 || rand == 2)
+    // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+    assert(set.remove(1))
+    // 2 已在集合中，所以返回 false 。
+    assert(!set.insert(2))
+    // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+    assert(set.getRandom() == 2)
+    assert(set.getRandom() == 2)
+    assert(set.getRandom() == 2)
+  }
+}
