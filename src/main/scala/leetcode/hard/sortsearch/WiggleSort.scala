8731241012dfb5414f3ac03ff30665c5d58d7811
Leibniz.Hu <leibnizhu@gmail.com>
2018-07-16 12:00:47 +0800

    Bugs fix, add test cases and comments

--- src/main/scala/leetcode/hard/sortsearch/WiggleSort.scala
+++ src/main/scala/leetcode/hard/sortsearch/WiggleSort.scala
@@ -9,17 +9,17 @@ package leetcode.hard.sortsearch
   */
 object WiggleSort {
   /**
-    * 外部调用时k传入nums的长度一半
+    * 外部调用时k传入nums的长度一半(考虑到奇偶长度的情况,应该是(length+1)/2)
     * 任选基准mid,通过filter把nums分为小于mid(下简称左),等于mid(下简称中),大于mid三部分(下简称右)
-    * 如果左的个数小于k,证明中位数应该在中或右里面,进一步判断中的范围是否覆盖k(中位数)位置,包含则返回mid,否则在右边递归找
-    * 如果左的个数大于等于k,则中位数在左里面,则递归在左边找
+    * 如果左的个数小于等于k,证明中位数应该在中或右里面,进一步判断中的范围是否覆盖k(中位数)位置,包含则返回mid,否则在右边递归找
+    * 如果左的个数大于k,则中位数在左里面,则递归在左边找
     */
   def findKth(nums: Array[Int], k: Int): Int = {
     val mid = nums(0) //比较的基准
     val left = nums.filter(_ < mid)
     val right = nums.filter(_ > mid)
     val midLeft = k - nums.length + right.length //左边和中间去掉k位之后剩余个数
-    if (left.length < k)
+    if (left.length <= k) //左的个数小于等于k,证明中位数应该在中或右里面
       if (midLeft <= 0) mid else findKth(right, midLeft)
     else
       findKth(left, k)
@@ -33,7 +33,7 @@ object WiggleSort {
     */
   def wiggleSort(nums: Array[Int]): Unit = {
     val len = nums.length
-    val mid = findKth(nums, len / 2)
+    val mid = findKth(nums, (len + 1) / 2)
     val res = Array.fill(len)(mid)
     val even = len % 2 == 0
     var l = if (even) len - 2 else 0
--- src/test/scala/leetcode/hard/sortsearch/WiggleSortTest.scala
+++ src/test/scala/leetcode/hard/sortsearch/WiggleSortTest.scala
@@ -17,6 +17,20 @@ class WiggleSortTest extends FunSuite {
     assert(check(arr))
   }
 
+  test("1") {
+    val arr = Array(1)
+    WiggleSort.wiggleSort(arr)
+    println(arr.toList)
+    assert(check(arr))
+  }
+
+  test("1,3,2,2,2,1,1,3,1,1,2") {
+    val arr = Array(1, 3, 2, 2, 2, 1, 1, 3, 1, 1, 2)
+    WiggleSort.wiggleSort(arr)
+    println(arr.toList)
+    assert(check(arr))
+  }
+
   def check(arr: Array[Int]): Boolean = (1 until arr.length) forall {
     case i if i % 2 == 1 => arr(i - 1) < arr(i)
     case i => arr(i - 1) > arr(i)
@@ -24,7 +38,9 @@ class WiggleSortTest extends FunSuite {
 
   test("middle") {
     assert(WiggleSort.findKth(Array(1, 5, 1, 1, 6, 4), 3) == 1)
-    assert(WiggleSort.findKth(Array(1, 3, 2, 2, 3, 1), 3) == 2)
+    assert(WiggleSort.findKth(Array(1, 3, 2, 2, 3, 1), 7/2) == 2)
     assert(WiggleSort.findKth(Array(1, 4, 5, 2, 3, 7, 6), 4) == 4)
+    assert(WiggleSort.findKth(Array(1), 0) == 1)
+    assert(WiggleSort.findKth(Array(1, 3, 2, 2, 2, 1, 1, 3, 1, 1, 2), 6) == 2)
   }
 }
