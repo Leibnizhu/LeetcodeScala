cc7993a35fd3b1dac87f8c73b81e44194170b8ee
Leibniz.Hu <leibnizhu@gmail.com>
2018-07-17 15:09:12 +0800

    Code optimization, delete useless function parameter

--- src/main/scala/leetcode/hard/sortsearch/SortedArrayMedian.scala
+++ src/main/scala/leetcode/hard/sortsearch/SortedArrayMedian.scala
@@ -14,25 +14,25 @@ object SortedArrayMedian {
     */
   def findMedianSortedArrays(nums1: Array[Int], nums2: Array[Int]): Double = {
     /**
-      * 在已排序的a(下标范围[al,ar])和b(下标范围[bl,br])查找如果合并后排在第k位置的数
+      * 在已排序的a(下标范围[as,length-1])和b(下标范围[bs,length-1])查找如果合并后排在第k位置的数
       */
-    def findKth(a: Array[Int], al: Int, ar: Int)(b: Array[Int], bl: Int, br: Int)(k: Int): Int = {
-      val la = ar - al + 1 //当前a的长度
-      val lb = br - bl + 1 //当前b的长度
+    def findKth(a: Array[Int], as: Int)(b: Array[Int], bs: Int)(k: Int): Int = {
+      val la = a.length - as //当前a的长度
+      val lb = b.length - bs //当前b的长度
       if (la == 0) { //短的一方长度为零了,要找的数必然在另一方,直接取即可
         b(k - 1)
       } else if (lb == 0) {
         a(k - 1)
       } else if (k == 1) { //找第一个,合并后必然是两方第一个元素较小者
-        Math.min(a(al), b(bl))
+        Math.min(a(as), b(bs))
       } else {
         var ia = Math.min(la, k >> 1) //本次迭代在a数组取多少个元素,最多只能取全部或k/2
         var ib = Math.min(lb, k >> 1) //本次迭代在b数组取多少个元素
-        if (la > lb) ia += k - ia - ib else ib += k - ia - ib
-        a(al + ia - 1) - b(bl + ib - 1) match {
-          case d if d < 0 => findKth(a, al + ia, ar)(b, bl, br)(k - ia) //a的数更小,说明a要往右边找才行
-          case d if d > 0 => findKth(a, al, ar)(b, bl + ib, br)(k - ib) //b的数更小,b要往右边找
-          case _ => a(al + ia - 1)
+        if (la > lb) ia = k - ib else ib = k - ia
+        a(as + ia - 1) - b(bs + ib - 1) match {
+          case d if d < 0 => findKth(a, as + ia)(b, bs)(k - ia) //a的数更小,说明a要往右边找才行
+          case d if d > 0 => findKth(a, as)(b, bs + ib)(k - ib) //b的数更小,b要往右边找
+          case _ => a(as + ia - 1)
         }
       }
     }
@@ -40,9 +40,9 @@ object SortedArrayMedian {
     val (m, n) = (nums1.length, nums2.length)
     val total = m + n
     if (total % 2 == 1) //奇数长度,取正中间的数
-      findKth(nums1, 0, m - 1)(nums2, 0, n - 1)((total + 1) / 2)
+      findKth(nums1, 0)(nums2, 0)((total + 1) / 2)
     else //偶数长度,取中间两个数平均值
-      (findKth(nums1, 0, m - 1)(nums2, 0, n - 1)(total / 2) +
-        findKth(nums1, 0, m - 1)(nums2, 0, n - 1)(total / 2 + 1)) / 2.0 //这里其实可以用前面的结果减少计算,可是边界条件控制起来比较麻烦,而且又要一个二分比较麻烦,还是直接调用findKth好了
+      (findKth(nums1, 0)(nums2, 0)(total / 2) +
+        findKth(nums1, 0)(nums2, 0)(total / 2 + 1)) / 2.0 //这里其实可以用前面的结果减少计算,可是边界条件控制起来比较麻烦,而且又要一个二分比较麻烦,还是直接调用findKth好了
   }
 }
