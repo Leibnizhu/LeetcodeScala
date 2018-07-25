b2ac087a90996b14da9cb3867df7c4c68b40acbc
Leibniz.Hu <leibnizhu@gmail.com>
2018-07-25 11:35:01 +0800

    Largest Rectangle In Histogram

---
+++ src/main/scala/leetcode/hard/other/LargestRectangleInHistogram.scala
@@ -0,0 +1,47 @@
+package leetcode.hard.other
+
+/**
+  * https://leetcode-cn.com/explore/interview/card/top-interview-questions-hard/63/others/169/
+  *
+  * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+  * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+  */
+object LargestRectangleInHistogram {
+  /**
+    * 跟之前MaxArea问题不一样,MaxArea只要拿到区间两边较小者即可计算面积(即O(1))时间
+    * 这题要计算面积的时候,要知道整个区间的最小值,亦即O(n)
+    * 而且这样就不能简单地判断两边缩小右边的条件了,只能暴力,因此不要(n*n*n)时间,会超时
+    * 问题在于确定矩形的左右边界,如果我们维护一个递增的数组,那么每个区间最小值都是左边,比较的时候遍历一次左值*(最右-左)即可
+    * 但是题目给出的柱状图不是单调增的,因此遍历源数组插入到递增数组,出现比数组最后一位更大的时候,需要合并,维护单调增
+    * 为了方便这样的操作,可以用栈来维护这个单调增数组
+    * 合并的操作其实就是计算当前单调增栈的最大面积, 就是出栈的值作为矩形高度,
+    * 右边界为遍历的下标i(不包含),左边界为新栈顶索引(栈空则宽度为i)
+    * 合并直到栈顶比i对应值小
+    */
+  def largestRectangleArea(heights: Array[Int]): Int = {
+    val len = heights.length
+    var stack = List[Int]() //Stack类已经废弃,源码注释里建议用var的List对象替代
+
+    /**
+      * 因为要递归,因此不能直接写在fold的参数里,要定义一个函数用来递归调用
+      * 满足栈单调增的入栈,否则合并前面的柱子计算面积,直到满足单调增为止
+      *
+      * @param curArea fold传递的中间结果值
+      * @param i       当前下标
+      * @return 本次迭代计算的面积
+      */
+    def calCurArea(curArea: Int, i: Int): Int =
+      if (stack.isEmpty || i < len && heights(stack.head) <= heights(i)) { //满足栈的单调增性,无需合并计算面积
+        stack ::= i //满足条件的可以入栈
+        curArea
+      } else {
+        val height = heights(stack.head) //当前要计算的矩形的高度
+        stack = stack.tail //相当于出栈
+        val newArea = Math.max(curArea, height * (if (stack.isEmpty) i else i - stack.head - 1)) //计算面积
+        calCurArea(newArea, i) //继续递归计算面积直到栈顶比当前值小,满足单调性,即合并成功
+      }
+
+    val totalArea = (0 /: (0 until len)) (calCurArea) //递归计算面积
+    calCurArea(totalArea, len) //补刀,处理最大矩形的右边界出现在数组最右边的情况
+  }
+}
---
+++ src/test/scala/leetcode/hard/other/LargestRectangleInHistogramTest.scala
@@ -0,0 +1,17 @@
+package leetcode.hard.other
+
+import org.scalatest.FunSuite
+
+class LargestRectangleInHistogramTest extends FunSuite {
+  test("sample1") {
+    assert(LargestRectangleInHistogram.largestRectangleArea(Array(2, 1, 5, 6, 2, 3)) == 10)
+  }
+
+  test("sample2") {
+    assert(LargestRectangleInHistogram.largestRectangleArea(Array(6, 2, 5, 4, 5, 1, 6)) == 12)
+  }
+
+  test("sample3") {
+    assert(LargestRectangleInHistogram.largestRectangleArea(Array(1)) == 1)
+  }
+}
