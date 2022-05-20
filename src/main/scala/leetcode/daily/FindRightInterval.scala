baf75de80f36392196a6e12b77b659694376f489
Leibniz.Hu <leibnizhu@gmail.com>
2022-05-20 11:17:52 +0800

    feat(): find-right-interval

---
+++ src/main/scala/leetcode/daily/FindRightInterval.scala
@@ -0,0 +1,32 @@
+package leetcode.daily
+
+import scala.annotation.tailrec
+
+
+/**
+  * 2022-05-20
+  * {@see <a href='https://leetcode.cn/problems/find-right-interval/'/> }
+  */
+object FindRightInterval {
+  def findRightInterval(intervals: Array[Array[Int]]): Array[Int] = {
+    //Array([左区间值,原下标])
+    val sortedLeftWithIndex = intervals.map(r => r(0)).zipWithIndex.sortBy(t => t._1)
+
+    //二分查找
+    @tailrec
+    def binarySearch(l: Int, r: Int, target: Int, result: Int): Int = {
+      if (l > r) {
+        result
+      } else {
+        val mid = (l + r) / 2
+        if (sortedLeftWithIndex(mid)._1 >= target) {
+          binarySearch(l, mid - 1, target, sortedLeftWithIndex(mid)._2)
+        } else {
+          binarySearch(mid + 1, r, target, result)
+        }
+      }
+    }
+
+    intervals.map(r => binarySearch(0, intervals.length - 1, r(1), -1))
+  }
+}
---
+++ src/test/scala/leetcode/daily/FindRightIntervalTest.scala
@@ -0,0 +1,17 @@
+package leetcode.daily
+
+import org.scalatest.FunSuite
+
+class FindRightIntervalTest extends FunSuite {
+  test("case1") {
+    assertResult(Array(-1))(FindRightInterval.findRightInterval(Array(Array(1, 2))))
+  }
+
+  test("case2") {
+    assertResult(Array(-1, 0, 1))(FindRightInterval.findRightInterval(Array(Array(3, 4), Array(2, 3), Array(1, 2))))
+  }
+
+  test("case3") {
+    assertResult(Array(-1, 2, -1))(FindRightInterval.findRightInterval(Array(Array(1, 4), Array(2, 3), Array(3, 4))))
+  }
+}
