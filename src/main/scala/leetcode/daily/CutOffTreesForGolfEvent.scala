f92d35101b79f4305e7ca45824a6133eefc643d9
Leibniz.Hu <leibnizhu@gmail.com>
2022-05-23 13:29:15 +0800

    test(): cut-off-trees-for-golf-event

--- src/main/scala/leetcode/daily/CutOffTreesForGolfEvent.scala
+++ src/main/scala/leetcode/daily/CutOffTreesForGolfEvent.scala
@@ -1,5 +1,7 @@
 package leetcode.daily
 
+import scala.collection.mutable
+
 /**
   * 2022-05-23
   * {@see <a href='https://leetcode.cn/problems/cut-off-trees-for-golf-event/'/> }
@@ -15,38 +17,36 @@ object CutOffTreesForGolfEvent {
     val trees = forest.zipWithIndex.flatMap(r => r._1.zipWithIndex.map(c => (c._1, (r._2, c._2)))) //转(数字,(行,列))
       .filter(_._1 > 1).sortBy(_._1).map(_._2) //过滤树，排序,座标
 
-    def bfs(fromX: Int, fromY: Int, toX: Int, toY: Int): Int = {
-      if (fromX == toX && fromY == toY) {
-        0
-      } else {
-        var step = 0
-        val queue = new scala.collection.mutable.Queue().enqueue((fromX, fromY))
-        val visited = Array.fill(m, n)(false)
-        visited(fromX)(fromY) = true
-        while (queue.nonEmpty) {
-          step += 1 //bfs一层算一步
-          val curQueueSize = queue.size //不能直接遍历，
-          for (i <- 1 to curQueueSize) {
-            val p = queue.dequeue()
-            for (direction <- directions) { //遍历4个方向
-              val (nextX, nextY) = (p._1 + direction._1, p._2 + direction._2) //该方向的下一个点
-              if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n && !visited(nextX)(nextY) && forest(nextX)(nextY) > 0) { //点座标合法性判断,及未被访问、不是墙
-                if (nextX == toX && nextY == toY) { //到达目的地
-                  return step
-                }
+    def bfs(queue: mutable.Queue[(Int, Int)], visited: Array[Array[Boolean]], toX: Int, toY: Int): Int = {
+      var step = 0
+      while (queue.nonEmpty) {
+        step += 1 //bfs一层算一步
+        val curQueueSize = queue.size //不能直接遍历，
+        for (i <- 1 to curQueueSize) {
+          val p = queue.dequeue()
+          for (direction <- directions) { //遍历4个方向
+            val (nextX, nextY) = (p._1 + direction._1, p._2 + direction._2) //该方向的下一个点
+            if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n && !visited(nextX)(nextY) && forest(nextX)(nextY) > 0) { //点座标合法性判断,及未被访问、不是墙
+              if (nextX == toX && nextY == toY) { //到达目的地
+                return step
+              } else {
                 queue.enqueue((nextX, nextY))
                 visited(nextX)(nextY) = true
               }
             }
           }
         }
-        -1
       }
+      -1
     }
 
     var (x, y, steps) = (0, 0, 0)
     for (tree <- trees) {
-      val curStep = bfs(x, y, tree._1, tree._2)
+      val curStep = if (x == tree._1 && y == tree._2) 0 else {
+        val visited = Array.fill(m, n)(false)
+        visited(x)(y) = true
+        bfs(new mutable.Queue().enqueue((x, y)), visited, tree._1, tree._2)
+      }
       if (curStep == -1) return -1 //有一步不能到达就应该退出
       steps += curStep
       x = tree._1
